ea4d7b9f69a6924c6687c71c0b7a1aae98ac1c8c
haritha <haritha.peryala@gmail.com>
2023-11-18 21:03:11 +0530

    Rebranding changes

--- application/src/main/java/com/tekcapzule/feedback/application/FeedbackApplication.java
+++ application/src/main/java/com/tekcapzule/feedback/application/FeedbackApplication.java
@@ -3,7 +3,7 @@ package com.tekcapzule.feedback.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.tekcapsule.feedback","com.tekcapsule.core"})
+@SpringBootApplication(scanBasePackages = {"com.tekcapzule.feedback","com.tekcapzule.core"})
 public class FeedbackApplication {
 
     public static void main(String[] args) {
